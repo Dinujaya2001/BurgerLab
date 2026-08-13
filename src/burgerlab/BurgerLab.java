4a03cfe0a70fcffb708b161e236035d45130e2ca
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2026-08-13 19:33:19 +0530

    Main Dashboard design create and so many updater in here

--- src/burgerlab/BurgerLab.java
+++ src/burgerlab/BurgerLab.java
@@ -4,6 +4,8 @@
  */
 package burgerlab;
 
+import burgerlab.view.MainDashBoardForm;
+
 /**
  *
  * @author Dinujaya Tharaka
@@ -14,7 +16,7 @@ public class BurgerLab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new MainDashBoardForm().setVisible(true);
     }
     
 }
